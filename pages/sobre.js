76773ad8062b9b2c3100260c4a3fb846c0b8deef
matheus <geof.matheus@gmail.com>
2022-01-27 00:18:32 -0300

    update

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,24 +1,24 @@
 import React from 'react'
 
 function Sobre() {
-  return(
-     <div className='container'>
-     <h3>Centro Acadêmico de Geofísica - CAGEOF</h3>
-     
-     <ul>
-       <li><strong>Data de Fundação:</strong> 01/05/2014</li>
-       <li><strong>Quantidade de Membros:</strong> 08 Titulares e 08 Suplentes</li>
-       <li><strong>Período da atual gestão:</strong> 01/12/21 Até 15/12/22</li>
-       <li><strong>Presidente:</strong> Isabelle de Menezes Cruz</li>
-       <li><strong>Secretária Geral:</strong> Jarlison Branches Licata</li>
-       <li><strong>Tesoureira:</strong> Savani dos Santos Azevedo</li>
-       <li><strong>Diretor de Relações e Ética:</strong> Antonio Marcos de Jesus</li>
-       <li><strong>Diretora de Esporte:</strong> Eliane Marques</li>
-       <li><strong>Diretor de Marketing:</strong> Antonio Marcos de Jesus</li>
-       <li><strong>Diretor de Eventos e Cultura:</strong> Yure dos Santos da Costa</li>
-     </ul>
-     </div>
-     )
+  return (
+    <div className='d-flex flex-column align-items-center'>
+      <h3>Centro Acadêmico de Geofísica - CAGEOF</h3>
+
+      <ul>
+        <li><strong>Data de Fundação:</strong> 01/05/2014</li>
+        <li><strong>Quantidade de Membros:</strong> 08 Titulares e 08 Suplentes</li>
+        <li><strong>Período da atual gestão:</strong> 01/12/21 Até 15/12/22</li>
+        <li><strong>Presidente:</strong> Isabelle de Menezes Cruz</li>
+        <li><strong>Secretária Geral:</strong> Jarlison Branches Licata</li>
+        <li><strong>Tesoureira:</strong> dos Santos Azevedo</li>
+        <li><strong>Diretor de Relações e Ética:</strong> Antonio Marcos de Jesus</li>
+        <li><strong>Diretora de Esporte:</strong> Eliane Marques</li>
+        <li><strong>Diretor de Marketing:</strong> Antonio Marcos de Jesus</li>
+        <li><strong>Diretor de Eventos e Cultura:</strong> Yure dos Santos da Costa</li>
+      </ul>
+    </div>
+  )
 }
 
 export default Sobre
