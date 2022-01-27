c55398e328988975206bf6a8fd4d069f28694f3c
matheus <geof.matheus@gmail.com>
2022-01-27 00:07:17 -0300

    add

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,41 +1,46 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import { ReactElement } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 const Header = () => {
     return (
-        <header className='p-3 bg-dark text-white'>
+        <header className='p-3 bg-light'>
             <div className='d-flex justify-content-around align-items-center'>
                 <Link href='/'>
                     <a className='text-decoration-none text-white'>
-                        LOGO
+                    <Image src='/logo.png' width={50}
+      height={50}/>
                     </a>
                 </Link>
 
                 <ul className='nav d-flex justify-content-around'>
                     <li>
                         <Link href='/'>
-                            <a className='nav-link px-2 text-white'>
-                                Home
+                            <a className='flex nav-link px-2 text-black'>
+                            <FontAwesomeIcon  icon="home" /> Home
                             </a>
                         </Link>
                     </li>
                     <li>
                         <Link href='/blog'>
-                            <a className='nav-link px-2 text-white'>
-                                Blog
+                            <a className='nav-link px-2 text-black'>
+                            <FontAwesomeIcon icon="rss" /> Blog
                             </a>
                         </Link>
                     </li>
                     <li>
                         <Link href='/contato'>
-                            <a className='nav-link px-2 text-white'>
-                                Contato
+                            <a className='nav-link px-2 text-black'>
+                            <FontAwesomeIcon icon="envelope" /> Contato
                             </a>
                         </Link>
                     </li>
 
                     <li>
                         <Link href='/sobre'>
-                            <a className='nav-link px-2 text-white'>
-                                Sobre Nos
+                            <a className='nav-link px-2 text-black'>
+                            <FontAwesomeIcon icon="user" /> Sobre Nos
                             </a>
                         </Link>
                     </li>
--- pages/sobre.js
+++ pages/sobre.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React from 'react'
 
 function Sobre() {
-  return <div>Sobre</div>;
+  return(
+     <div className='container'>
+     <h1>Centro Acadêmico de Geofísica - CAGEOF</h1>
+     
+     <ul>
+       <li><strong>Data de Fundação:</strong> 01/05/2014</li>
+       <li><strong>Quantidade de Membros:</strong> 08 Titulares e 08 Suplentes</li>
+       <li><strong>Período da atual gestão:</strong> 01/12/21 Até 15/12/22</li>
+       <li><strong>Presidente:</strong> Isabelle de Menezes Cruz</li>
+       <li><strong>Secretária Geral:</strong> Jarlison Branches Licata</li>
+       <li><strong>Tesoureira:</strong> Savani dos Santos Azevedo</li>
+       <li><strong>Diretor de Relações e Ética:</strong> Antonio Marcos de Jesus</li>
+       <li><strong>Diretora de Esporte:</strong> Eliane Marques</li>
+       <li><strong>Diretor de Marketing:</strong> Antonio Marcos de Jesus</li>
+       <li><strong>Diretor de Eventos e Cultura:</strong> Yure dos Santos da Costa</li>
+     </ul>
+     </div>
+     )
 }
 
-export default Sobre;
+export default Sobre
