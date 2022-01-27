761c9d4e7c27c29313daae8aa4054320146171e4
matheus <geof.matheus@gmail.com>
2022-01-26 23:15:37 -0300

    footer modificado

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 function Footer() {
     return (
@@ -7,24 +8,32 @@ function Footer() {
             <footer className='py-3 my-4'>
                 <ul className='nav justify-content-center border-bottom pb-3 mb-3'>
                     <li className='nav-item'>
-                        <a href='#' className='nav-link px-2 text-muted'>
-                            Home
-                        </a>
+                        <Link href='/'>
+                            <a className='nav-link px-2 text-muted'>
+                                Home
+                            </a>
+                        </Link>
                     </li>
                     <li className='nav-item'>
-                        <a href='#' className='nav-link px-2 text-muted'>
-                            Blog
-                        </a>
+                    <Link href='/blog'>
+                            <a className='nav-link px-2 text-muted'>
+                                Blog
+                            </a>
+                        </Link>
                     </li>
                     <li className='nav-item'>
-                        <a href='#' className='nav-link px-2 text-muted'>
-                            Contato
-                        </a>
+                    <Link href='/contato'>
+                            <a className='nav-link px-2 text-muted'>
+                                Contato
+                            </a>
+                        </Link>
                     </li>
                     <li className='nav-item'>
-                        <a href='#' className='nav-link px-2 text-muted'>
-                            Sobre Nos
-                        </a>
+                    <Link href='/sobre'>
+                            <a className='nav-link px-2 text-muted'>
+                                Sobre Nos
+                            </a>
+                        </Link>
                     </li>
                 </ul>
                 <p className='text-center text-muted'>© 2022 Cageof</p>
